a35ec479a62f891baa212a2c9eefad76497777ae
VinayagamD <vinayagam.d.ganesh@gmail.com>
2022-01-18 18:42:26 +0530

    Intial Java Class Basic Code: Day1

---
+++ BasicCode/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+  public static void main(String[] args) {
+      System.out.println("Java Succesfully ran");
+  }
+}
+
+class Test1 {
+
+}
+
+class Test2{
+
+}
+
+class Test3{
+
+}
