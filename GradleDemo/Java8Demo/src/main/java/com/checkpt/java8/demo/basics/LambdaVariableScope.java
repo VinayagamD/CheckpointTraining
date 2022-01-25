4c64f63c5ecdd8c76380b907c1a0bbe9d0b24d92
VinayagamD <vinayagam.d.ganesh@gmail.com>
2022-01-25 22:44:58 +0530

    Day 4 Fixed HTTP Client Setup

--- GradleDemo/Java8Demo/src/main/java/com/checkpt/java8/demo/basics/LambdaVariableScope.java
+++ GradleDemo/Java8Demo/src/main/java/com/checkpt/java8/demo/basics/LambdaVariableScope.java
@@ -4,10 +4,6 @@ import com.checkpt.java8.demo.basics.interfaces.PostsHandler;
 import com.checkpt.java8.demo.basics.interfaces.VariableScope;
 import com.checkpt.java8.demo.data.Post;
 import com.checkpt.java8.demo.data.Student;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -18,6 +14,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class LambdaVariableScope {
     Student student = new Student();
@@ -29,29 +26,20 @@ public class LambdaVariableScope {
             student.setName("Test Name");
         };
 //        student = new Student();
-        Request request = new Request.Builder()
-                .url("https://jsonplaceholder.typicode.com/posts")
-                .build();
 
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Call call = okHttpClient.newCall(request);
-        Response response = call.execute();
-
-
-       /* HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create())
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
                 .build();
         HttpClient client = HttpClient.newBuilder().build();
         HttpResponse<String> response  = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
         PostsHandler postsHandler  = (json) -> {
             JSONArray jsonArray = new JSONArray(json);
-            return jsonArray.toList().stream().map(data -> {
-                JSONObject jsonObject = new JSONObject(data);
-                return Post.fromJson(jsonObject);
-            }).collect(Collectors.toList());
+            return StreamSupport.stream(jsonArray.spliterator(),false)
+                    .map(e -> Post.fromJson((JSONObject) e)).collect(Collectors.toList());
         };
-        List<Post>  posts = postsHandler.handlePosts(response.body()) ;*/
+        List<Post>  posts = postsHandler.handlePosts(response.body()) ;
+        posts.forEach(System.out::println);
 
     }
 
